ebcf047d63c227153a1555a9f531c91865bbc29e
Max Stevenson <maxstevenson@msn.com>
2021-01-05 09:40:47 -0600

    - Added main wp loop in frontpage. - Added dynamic header content in header template.

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+
+  <p class="copyright">Max Stevenson</p>
+
+</footer>
+
+</div>
+
 <?php
-	wp_footer();
-	?>
+wp_footer();
+?>
 
 </body>
-
 </html>
