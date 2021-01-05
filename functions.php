896fe1493167693db18646a6c8c82f03450cfebb
Max Stevenson <maxstevenson@msn.com>
2021-01-05 09:07:16 -0600

    - Added function for dynamic title on pages.

--- functions.php
+++ functions.php
@@ -1,4 +1,10 @@
 <?php
+
+function custom_theme_support() {
+  add_theme_support("title-tag");
+}
+add_action("after_setup_theme", "custom_theme_support");
+
 function custom_register_styles() {
   $version = wp_get_theme()->get("Version");
   wp_enqueue_style("trialtheme-style", get_template_directory_uri() . "/style.css", array("trialtheme-bootstrap"), $version, "all");
--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <head>
-	<title>Blog Site Template</title>
 	<!-- Meta -->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
