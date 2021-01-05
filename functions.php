f7754905bcd52d8ce73f686df6e61e9a60af1216
Max Stevenson <maxstevenson@msn.com>
2021-01-05 09:03:25 -0600

    - Fixed link for styles file in enqueue function. - Dynamically included header and footer in frontpage.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function custom_register_styles() {
   $version = wp_get_theme()->get("Version");
-  wp_enqueue_style("trialtheme-style", get_template_directory_uri() . "/assets/css/style.css", array("trialtheme-bootstrap"), $version, "all");
+  wp_enqueue_style("trialtheme-style", get_template_directory_uri() . "/style.css", array("trialtheme-bootstrap"), $version, "all");
   wp_enqueue_style("trialtheme-bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "4.4.1", "all");
   wp_enqueue_style("trialtheme-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), "5.13.0", "all");
 }
